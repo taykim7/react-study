4607998419aa76050f00f87f3ae59ab16dc13091
taykim7 <th_77@naver.com>
2024-11-05 21:26:45 +0900

    왕기초 js

---
+++ onebite-react1/section01/section01.js
@@ -0,0 +1,18 @@
+// 변수와 상수
+let age;
+age = 30;
+const birth = 714
+console.log(age)
+console.log(birth)
+
+// 명명 규칙들
+let $_name = '네이밍으로 $, _ 만 사용할 수 있음';
+let name1 = '네이밍으로 숫자로 시작할 수 없음'
+console.log($_name)
+console.log(name1)
+
+// 네이밍은 스윗하게 ^^
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount- refundCount;
+console.log(totalSalesCount)
