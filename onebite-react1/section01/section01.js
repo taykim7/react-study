6983ef5b1414a992f618f3fdbdc5590cd0b26aea
taykim7 <th_77@naver.com>
2024-11-07 21:50:45 +0900

    js basic

--- onebite-react1/section01/section01.js
+++ onebite-react1/section01/section01.js
@@ -187,3 +187,12 @@ const student = {
   }
 }
 student.sayHi();
+
+// 배열 - 여러개의 값을 순차적으로 담을 수 있는 자료형
+let arrA = new Array(); // 배열 생성자 방식
+let arrB = []; // 배열 리터럴 방식 (대부분 사용)
+let arrC = [1, 2, 3, true, 'hi', null, () => {}]
+
+// 배열 요소 접근, 수정
+let item1 = arrC[0];
+arrC[1] = 10;
