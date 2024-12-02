03b9470abf064cf163f44c54d24e58ba46f0b2c1
taykim7 <th_77@naver.com>
2024-12-02 21:31:53 +0900

    자식 컴포넌트 생성

--- onebite-react1/section05/src/App.jsx
+++ onebite-react1/section05/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 
-function App() {
+const Header = () => {
+  return (
+    <header>
+      <h1>헤더</h1>
+    </header>
+  )
+};
 
+function App() {
   return (
     <>
+      <Header />
       <h1>리액트!</h1>
     </>
   )
-}
+};
 
-export default App
+export default App;
