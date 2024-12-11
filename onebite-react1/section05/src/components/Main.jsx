b9aa10c36f121c8315b499a7e3f94cc0d0ac8953
taykim7 <th_77@naver.com>
2024-12-11 21:43:36 +0900

    JSX 문법 실습

--- onebite-react1/section05/src/components/Main.jsx
+++ onebite-react1/section05/src/components/Main.jsx
@@ -1,9 +1,35 @@
+import "./Main.css";
+
 const Main = () => {
-  return (
-    <main>
-      <h1>메인</h1>
-    </main>
-  )
+  const user = {
+    name: '김테이',
+    isLogin: true,
+  }
+
+  // return (
+  //   <>
+  //     { user.isLogin ? (<div>로그아웃</div>) : (<div>로그인</div>) }
+  //   </>
+  // )
+
+  // 조건문 + 스타일
+  // if (user.isLogin) {
+  //   return <div style={{
+  //     backgroundColor: 'red',
+  //     borderBottom: '5px solid blue'
+  //   }}>로그아웃</div>;
+  // } else {
+  //   return <div>로그인</div>
+  // }
+
+  // css 분리
+  // 자바스크립트의 예약어인 class를 사용하지 못하여 className을 통해 지정한다.
+  if (user.isLogin) {
+    return <div className="logout">로그아웃</div>;
+  } else {
+    return <div>로그인</div>
+  }
+
 }
 
 export default Main;
