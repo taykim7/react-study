51e7f6a86d9463592d449d7c88b3f9c267fac81d
taykim7 <th_77@naver.com>
2024-12-18 22:11:56 +0900

    State

--- onebite-react1/section05/src/App.jsx
+++ onebite-react1/section05/src/App.jsx
@@ -1,24 +1,24 @@
 import './App.css'
-// import Header from './components/Header';
-// import Main from './components/Main';
-// import Footer from './components/Footer';
-import Button from './components/Button';
+import { useState } from 'react';
 
-const buttonProps = {
-  text: '기타',
-  color: 'blue'
-}
 function App() {
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState('OFF');
+
   return (
     <>
-      <Button text={'메일'} color={"red"}  />
-      <Button text={'카페'} />
-      <Button text={'블로그'} />
-      <Button {...buttonProps} />
-      <Button text={'html'}>
-        <div>자식요소</div>
-      </Button>
-      <Button />
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => {
+          setCount(count + 1);
+        }}>더해라</button>
+      </div>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={()=>{
+          setLight(light==='ON' ? 'OFF' : 'ON')
+        }}>{light === 'ON' ? '끄기' : '켜기'}</button>
+      </div>
     </>
   )
 };
