7eebcf4032c9fa256d926861a46536cf9cbfa2b3
taykim7 <th_77@naver.com>
2025-02-10 22:46:43 +0900

    카운터앱
    - 기본 UI 추가

--- onebite-react1/section06/src/App.jsx
+++ onebite-react1/section06/src/App.jsx
@@ -1,11 +1,18 @@
-
 import './App.css'
+import Viewer from './components/Viewer'
+import Controller from './components/Controller'
 
 function App() {
   return (
-    <>
-      카운터앱
-    </>
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
   )
 }
 
