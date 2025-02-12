80106023ab5cb4932a965e261ec2d38fc1d41ca7
taykim7 <th_77@naver.com>
2025-02-12 21:29:29 +0900

    useEffect

--- onebite-react1/section07/src/App.jsx
+++ onebite-react1/section07/src/App.jsx
@@ -1,16 +1,28 @@
 import './App.css'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState('');
+
+
+  useEffect(()=>{console.log('변경됨')}, [count, input]);
+  // [count]는 useEffect가 의존하기 때문에 의존성 배열이라고 함
+  // dependency array 줄여서 deps
+
   const onClickButton = (value) => {
     setCount(count+value);
   }
   return (
     <div className='App'>
       <h1>Simple Counter</h1>
+      <section>
+        <input value={input} onChange={(e)=>{
+          setInput(e.target.value);
+        }}/>
+      </section>
       <section>
         <Viewer count={count} />
       </section>
