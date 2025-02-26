cbc1bccd8fbb77b26a43b87129ebaa767842f653
taykim7 <th_77@naver.com>
2025-02-26 23:16:16 +0900

    투두앱
    - App 컴포넌트로부터 전달받은 onCreate를 수행하는 onSubmit 메서드 추가
    - onSubmit 메서드에서 우선 데이터 검사
    - onCreate 후 데이터 비우기
    - enter에도 반응할 수 있도록 이벤트핸들러 등록

--- onebite-react1/section08/src/components/Editor.jsx
+++ onebite-react1/section08/src/components/Editor.jsx
@@ -1,10 +1,48 @@
 import './Editor.css';
+import { useState, useRef } from 'react';
 
-const Editor = () => {
+const Editor = ({onCreate}) => {
+  const [content, setContent] = useState('');
+  
+  // focus를 위한 레퍼런스
+  const contentRef = useRef();
+
+  // 이벤트 핸들러
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  }
+  
+  // 이벤트 핸들러 (key)
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  }
+
+  const onSubmit = () =>{
+    // 데이터 검사
+    if (content === '') {
+      contentRef.current.focus();
+      return;
+    }
+
+    // 데이터 추가
+    onCreate(content);
+    
+    // 데이터 초기화
+    setContent('');
+  }
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 TODO ..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 TODO ..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   )
 }
