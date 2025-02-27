4ffdeb66a0313dd5bff92f7c414995edc53fc7e2
taykim7 <th_77@naver.com>
2025-02-27 23:15:07 +0900

    투두앱
    - 리스트 출력

--- onebite-react1/section08/src/App.jsx
+++ onebite-react1/section08/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
     <div className='App'>
       <Header/>
       <Editor onCreate={onCreate}/>
-      <List/>
+      <List todos={todos}/>
     </div>
   )
 }
--- onebite-react1/section08/src/components/List.jsx
+++ onebite-react1/section08/src/components/List.jsx
@@ -1,15 +1,15 @@
 import './List.css'
 import TodoItem from './TodoItem';
 
-const List = () => {
+const List = ({todos}) => {
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
       <input placeholder="검색어를 입력하세요"/>
       <div className='todos_wrapper'>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map((todo)=>{
+          return <TodoItem {...todo} key={todo.id}/>
+        })}
       </div>
     </div>
   )
