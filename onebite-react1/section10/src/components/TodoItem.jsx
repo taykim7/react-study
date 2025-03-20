5745a00c709c0c61e83c54bc6d19f05c05f81195
taykim7 <th_77@naver.com>
2025-03-20 22:44:18 +0900

    memo의 두번째 인수를 활용한 리렌더링 방지
    - memo는 얕은 비교를 하기 때문에 객체 타입은 무조건 다르다고 판단하여 리렌더링된다.
    - 그래서 두번째 인수로 콜백함수를 전달하여 최적화 기능을 커스텀한다.
    - 바꼈을 때 리렌더링이 필요한 항목들만 서로 비교하여 false를 반환
    - 그 외에는 true로 반환하여 메모이제이션 한다.

--- onebite-react1/section10/src/components/TodoItem.jsx
+++ onebite-react1/section10/src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import './TodoItem.css'
+import { memo } from 'react';
 
 const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
 
@@ -25,4 +26,24 @@ const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
   )
 }
 
-export default TodoItem;
+// export default memo(TodoItem); ==> 최적화 실패
+
+// memo 메서드는 현재와 과거를 얕은 비교를 하기 때문에
+// 함수와 같은 객체타입은 무조건 다르다고 판단을 함.
+// 즉, 객체타입을 props로 받는 컴포넌트는 memo도 최적화가 안됨
+// 방법1. props로 보내는 함수들을 메모이제이션한다. ==> useCallback활용
+// 방법2. memo함수에 두번째 인수로 콜백함수를 전달하여 최적화기능을 커스텀한다.
+
+export default memo(TodoItem, (prevProps, nextProps)=>{
+  // 반환값에 따라, Props가 바뀌었는지 판단
+  // true일땐 props가 바뀌지 않음
+  // false일땐 props가 바뀐 것
+
+  // id, isDone, content, date가 다를때만 리렌더링
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  return true;
+});
