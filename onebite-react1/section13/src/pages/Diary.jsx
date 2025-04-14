0a7b120643f5c2300ed0674c634807c128d23a44
taykim7 <th_77@naver.com>
2025-04-14 22:01:34 +0900

    파라미터를 통한 동적경로

--- onebite-react1/section13/src/pages/Diary.jsx
+++ onebite-react1/section13/src/pages/Diary.jsx
@@ -1,5 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
-  return <div>Diary</div>;
+  // 현재 브라우저에 명시한 파라미터를 가져올 수 있음
+  const params = useParams();
+  return <div>{params.id}번 Diary</div>;
 }
 
 export default Diary;
