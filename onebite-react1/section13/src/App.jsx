ef135c838512485a76e53d92af9d68c9a5a9c3ef
taykim7 <th_77@naver.com>
2025-04-15 22:11:58 +0900

    폰트, 이미지 적용

--- onebite-react1/section13/src/App.jsx
+++ onebite-react1/section13/src/App.jsx
@@ -5,6 +5,12 @@ import New from './pages/New'
 import Diary from './pages/Diary'
 import Notfound from './pages/Notfound'
 
+import emotion1 from './assets/emotion1.png';
+import emotion2 from './assets/emotion2.png';
+import emotion3 from './assets/emotion3.png';
+import emotion4 from './assets/emotion4.png';
+import emotion5 from './assets/emotion5.png';
+
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
@@ -16,6 +22,13 @@ function App() {
   }
   return (
     <>
+      <div>
+        <img src={emotion1} />
+        <img src={emotion2} />
+        <img src={emotion3} />
+        <img src={emotion4} />
+        <img src={emotion5} />
+      </div>
       <div>
         <Link to={"/"} >Home</Link>
         <Link to={"/new"} >New</Link>
