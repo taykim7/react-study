b7c6a2b481c6848a3cd8181b473ee612695cede0
taykim7 <th_77@naver.com>
2025-04-16 23:00:59 +0900

    기타 수정

--- onebite-react1/section13/src/pages/Home.jsx
+++ onebite-react1/section13/src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 const Home = () => {
   const [params, setParams ] = useSearchParams();
-  console.log(params.get("value"));
-  
   return <div>Home</div>;
 }
 
