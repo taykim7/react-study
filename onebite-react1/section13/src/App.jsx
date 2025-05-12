874d57068d7766f52c6d99899c88e1c7b930f1b8
taykim7 <th_77@naver.com>
2025-05-12 22:41:04 +0900

    Edit 컴포넌트 추가

--- onebite-react1/section13/src/App.jsx
+++ onebite-react1/section13/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home'
 import New from './pages/New'
 import Diary from './pages/Diary'
 import Notfound from './pages/Notfound'
+import Edit from './pages/Edit'
+
 import Button from './components/Button'
 import Header from './components/Header'
 
@@ -12,6 +14,7 @@ import { getEmotionImage } from './util/get-emotion-image'
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+// 4. "/edit" : 일기를 수정하는 Edit 페이지
 
 function App() {
   const nav = useNavigate();
@@ -47,6 +50,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
