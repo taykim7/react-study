8ee5eec05cf941187152796e18a32494a3827aec
taykim7 <th_77@naver.com>
2025-05-12 22:43:30 +0900

    불필요한 코드 제거

--- onebite-react1/section13/src/App.jsx
+++ onebite-react1/section13/src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css'
-import { Routes, Route, Link, useNavigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import New from './pages/New'
 import Diary from './pages/Diary'
 import Notfound from './pages/Notfound'
 import Edit from './pages/Edit'
-
-import Button from './components/Button'
-import Header from './components/Header'
-
-import { getEmotionImage } from './util/get-emotion-image'
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -17,10 +12,6 @@ import { getEmotionImage } from './util/get-emotion-image'
 // 4. "/edit" : 일기를 수정하는 Edit 페이지
 
 function App() {
-  const nav = useNavigate();
-  const onClickButton = () => {
-    nav('new');
-  }
   return (
     <>
       <Routes>
