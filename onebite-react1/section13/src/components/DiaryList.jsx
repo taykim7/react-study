3a4cb64207ea86dcf951b678a4925490c8c244ce
taykim7 <th_77@naver.com>
2025-05-15 22:53:31 +0900

    데이터 정렬 기능 추가
    - 정렬 기준을 담은 state 추가
    - select에 이벤트핸들러를 등록하여 정렬 state 값을 변경함
    - 기존에 그냥 data를 사용하지 않고 toSorted를 통해 현재 state에 따라 정렬된 data를 사용하도록 변경

--- onebite-react1/section13/src/components/DiaryList.jsx
+++ onebite-react1/section13/src/components/DiaryList.jsx
@@ -2,21 +2,40 @@ import './DiaryList.css'
 import Button from "./Button";
 import DiaryItem from './DiaryItem';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const DiaryList = ({data}) => {
   // 페이지 이동
   const nav = useNavigate();
+  // 정렬 스테이트
+  const [sortType, setSortType] = useState('latest');
+  // 정렬 이벤트핸들러
+  const onChageSortType = (e) => {
+    setSortType(e.target.value);
+  }
+  // 정렬 기능
+  const getSortedDate = () => {
+    return data.toSorted((a, b) => {
+      if(sortType === 'oldest') {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  }
+  const sortedData = getSortedDate();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChageSortType}>
           <option value={'latest'}>최신순</option>
           <option value={'oldest'}>오래된 순</option>
         </select>
         <Button onClick={() => nav('/new')} text={'새 일기 쓰기'} type={'POSITIVE'} />
       </div>
       <div className="list_wrapper">
-        {data.map((item) => <DiaryItem key={item.id} {...item} />)}
+        {sortedData.map((item) => <DiaryItem key={item.id} {...item} />)}
       </div>
     </div>
   );
