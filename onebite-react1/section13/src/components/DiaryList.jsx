6676eeb8454c88c91ed3873bdf87c9aa4327bd9f
taykim7 <th_77@naver.com>
2025-05-15 22:45:58 +0900

    페이지이동2

--- onebite-react1/section13/src/components/DiaryList.jsx
+++ onebite-react1/section13/src/components/DiaryList.jsx
@@ -1,8 +1,11 @@
 import './DiaryList.css'
 import Button from "./Button";
 import DiaryItem from './DiaryItem';
+import { useNavigate } from 'react-router-dom';
 
 const DiaryList = ({data}) => {
+  // 페이지 이동
+  const nav = useNavigate();
   return (
     <div className="DiaryList">
       <div className="menu_bar">
@@ -10,7 +13,7 @@ const DiaryList = ({data}) => {
           <option value={'latest'}>최신순</option>
           <option value={'oldest'}>오래된 순</option>
         </select>
-        <Button text={'새 일기 쓰기'} type={'POSITIVE'} />
+        <Button onClick={() => nav('/new')} text={'새 일기 쓰기'} type={'POSITIVE'} />
       </div>
       <div className="list_wrapper">
         {data.map((item) => <DiaryItem key={item.id} {...item} />)}
