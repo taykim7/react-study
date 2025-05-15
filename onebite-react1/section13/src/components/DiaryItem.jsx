39ceeef63aa4f65c3b26e5ac1956f9134b4a365a
taykim7 <th_77@naver.com>
2025-05-15 22:44:16 +0900

    각각의 일기 클릭시 페이지 이동
    - useNavigate 활용

--- onebite-react1/section13/src/components/DiaryItem.jsx
+++ onebite-react1/section13/src/components/DiaryItem.jsx
@@ -1,15 +1,23 @@
 import { getEmotionImage } from '../util/get-emotion-image'
 import Button from './Button';
 import './DiaryItem.css'
+import { useNavigate } from 'react-router-dom';
 
 const DiaryItem = ({id, emotionId, createdDate, content}) => {
 
+  // 페이지 이동
+  const nav = useNavigate();
+
   return (
     <div className="DiaryItem">
-      <div className={`img_section img_section_${emotionId}`}>
+      <div
+      onClick={() => nav(`/diary/${id}`)}
+      className={`img_section img_section_${emotionId}`}>
         <img src={getEmotionImage(emotionId)} />
       </div>
-      <div className="info_section">
+      <div
+      onClick={() => nav(`/diary/${id}`)}
+      className="info_section">
         <div className='created_date'>
           {new Date(createdDate).toLocaleDateString()}
         </div>
@@ -18,7 +26,10 @@ const DiaryItem = ({id, emotionId, createdDate, content}) => {
         </div>
       </div>
       <div className="button_section">
-        <Button text={'수정하기'} />
+        <Button
+          onClick={() => nav(`/edit/${id}`)}
+          text={'수정하기'}
+        />
       </div>
     </div>
   );
