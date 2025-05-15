d2eb3fdba5c818e3206664099979a15bb37491b7
taykim7 <th_77@naver.com>
2025-05-15 22:12:11 +0900

    헤더 기능 구현
    - 날짜 state 추가
    - 다음달로 이동
    - 이전달로 이동

--- onebite-react1/section13/src/pages/Home.jsx
+++ onebite-react1/section13/src/pages/Home.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 import Header from "../components/Header";
 import Button from '../components/Button';
 import DiaryList from "../components/DiaryList";
 
 const Home = () => {
+  // 날짜
+  const [pivotDate, setPivotDate] = useState(new Date());
+  // 다음 달
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  // 이전 달
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   return (
   <div>
-    <Header title={'2025년 5월'} 
-      leftChild={<Button text={'<'} />}
-      rightChild={<Button text={'>'} />}
+    <Header
+      title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`} 
+      leftChild={<Button onClick={onDecreaseMonth} text={'<'} />}
+      rightChild={<Button onClick={onIncreaseMonth} text={'>'} />}
     />
     <DiaryList />
   </div>
