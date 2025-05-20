9fbf0152cd231fb7198ae80763861e108516efd8
taykim7 <th_77@naver.com>
2025-05-20 21:35:44 +0900

    새 일기 쓰기 헤더 구성

--- onebite-react1/section13/src/pages/New.jsx
+++ onebite-react1/section13/src/pages/New.jsx
@@ -1,5 +1,13 @@
+import Header from '../components/Header';
+import Button from '../components/Button';
+
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div>
+      <Header title={'새 일기 쓰기'}
+      leftChild={<Button text={'< 뒤로 가기'} />} />
+    </div>
+  );
 }
 
 export default New;
