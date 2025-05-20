4d684273a9a4dce39a48bf9f74cde26934323142
taykim7 <th_77@naver.com>
2025-05-20 21:40:56 +0900

    에디터 개발 중
    - 에디터 화면에 추가
    - 헤더부분 UI 구현

--- onebite-react1/section13/src/components/Editor.jsx
+++ onebite-react1/section13/src/components/Editor.jsx
@@ -1,7 +1,17 @@
 import './Editor.css';
 
 const Editor = () => {
-  return <div>Editor</div>
+  return (
+    <div className='Editor'>
+      <section className='date_section'>
+        <h4>오늘의 날짜</h4>
+        <input type='date' />
+      </section>
+      <section className='emotion_section'></section>
+      <section className='content_section'></section>
+      <section className='button_section'></section>
+    </div>
+  )
 }
 
 export default Editor
