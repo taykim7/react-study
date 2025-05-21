2d7f0a258a8e5e81597980b425a48d426fb86647
taykim7 <th_77@naver.com>
2025-05-21 22:42:54 +0900

    새일기쓰기 화면 뒤로가기 구현
    - useNavigate 활용

--- onebite-react1/section13/src/pages/New.jsx
+++ onebite-react1/section13/src/pages/New.jsx
@@ -1,12 +1,20 @@
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Editor from '../components/Editor';
+import { useNavigate } from 'react-router-dom';
 
 const New = () => {
+  const nav = useNavigate();
+  
   return (
     <div>
       <Header title={'새 일기 쓰기'}
-      leftChild={<Button text={'< 뒤로 가기'}/>}
+      leftChild={
+        <Button
+          onClick={() => nav(-1)}
+          text={'< 뒤로 가기'}
+        />
+      }
     />
     <Editor />
     </div>
