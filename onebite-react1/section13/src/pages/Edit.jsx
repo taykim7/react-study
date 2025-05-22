e9fb23f0b9fa3088cdc92bef79785e4aa8acdff5
taykim7 <th_77@naver.com>
2025-05-22 23:14:21 +0900

    수정하면 에디터 UI 추가

--- onebite-react1/section13/src/pages/Edit.jsx
+++ onebite-react1/section13/src/pages/Edit.jsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
 import Header from '../components/Header';
 import Button from '../components/Button';
+import Editor from '../components/Editor';
 
 const Edit = () => {
   const params = useParams();
-  
+
   return (
     <div>
       <Header
@@ -12,6 +13,7 @@ const Edit = () => {
         leftChild={<Button text={'< 뒤로 가기'} />}
         rightChild={<Button text={'삭제하기'} type="NEGATIVE" />}
       />
+      <Editor />
     </div>
   );
 };
