1986cc7ddf8bc518f5780a088f5084160087fdd2
taykim7 <th_77@naver.com>
2025-05-22 23:21:24 +0900

    수정화면 뒤로가기, 삭제하기 기능 구현
    - 뒤로가기는 그냥 네비게이션 이용
    - 삭제하기는 context 메서드 공급받아서 이용

--- onebite-react1/section13/src/pages/Edit.jsx
+++ onebite-react1/section13/src/pages/Edit.jsx
@@ -1,17 +1,46 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Editor from '../components/Editor';
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const Edit = () => {
   const params = useParams();
+  const nav = useNavigate();
+
+  // 삭제 메서드 공급받기 
+  const {onDelete} = useContext(DiaryDispatchContext);
+
+  // 이벤트핸들러
+  const onClickDelete = () => {
+    // 삭제 확인
+    if (
+      window.confirm('일기를 정말 삭제할까요?')
+    ) {
+      // 일기 삭제
+      onDelete(params.id);
+      nav('/', { replace: true })
+    }
+  };
 
   return (
     <div>
       <Header
         title={'일기 수정하기'}
-        leftChild={<Button text={'< 뒤로 가기'} />}
-        rightChild={<Button text={'삭제하기'} type="NEGATIVE" />}
+        leftChild={
+          <Button
+            onClick={() => nav(-1)}
+            text={'< 뒤로 가기'}
+          />
+        }
+        rightChild={
+          <Button
+            onClick={onClickDelete}
+            text={'삭제하기'}
+            type="NEGATIVE"
+          />
+        }
       />
       <Editor />
     </div>
