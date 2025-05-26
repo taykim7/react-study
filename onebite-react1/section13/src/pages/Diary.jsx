24b18de603416251c6e2c12fd1e2076eb1f871ef
taykim7 <th_77@naver.com>
2025-05-26 22:09:44 +0900

    리스트 클릭하면 해당 id 일기 화면으로 이동, 뒤로가기 구현

--- onebite-react1/section13/src/pages/Diary.jsx
+++ onebite-react1/section13/src/pages/Diary.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Viewer from '../components/Viewer';
@@ -6,12 +6,13 @@ import Viewer from '../components/Viewer';
 const Diary = () => {
   // 현재 브라우저에 명시한 파라미터를 가져올 수 있음
   const params = useParams();
-  
+  const nav = useNavigate();
+
   return <div>
     <Header
       title={'yyyy-mm-dd 기록'}
-      leftChild={<Button text={'< 뒤로 가기'}/>}
-      rightChild={<Button text={'수정하기'} />}
+      leftChild={<Button onClick={()=>nav(-1)} text={'< 뒤로 가기'}/>}
+      rightChild={<Button onClick={()=>nav(`/edit/${params.id}`)} text={'수정하기'} />}
     />
     <Viewer />
   </div>;
