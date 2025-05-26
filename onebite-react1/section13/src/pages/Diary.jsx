2971a1d4d200b2907800c514cc71188812151184
taykim7 <th_77@naver.com>
2025-05-26 22:06:34 +0900

    일기 화면 UI 구현

--- onebite-react1/section13/src/pages/Diary.jsx
+++ onebite-react1/section13/src/pages/Diary.jsx
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom";
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Viewer from '../components/Viewer';
 
 const Diary = () => {
   // 현재 브라우저에 명시한 파라미터를 가져올 수 있음
   const params = useParams();
-  return <div>{params.id}번 Diary</div>;
+  
+  return <div>
+    <Header
+      title={'yyyy-mm-dd 기록'}
+      leftChild={<Button text={'< 뒤로 가기'}/>}
+      rightChild={<Button text={'수정하기'} />}
+    />
+    <Viewer />
+  </div>;
 }
 
 export default Diary;
