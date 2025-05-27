1aff7dac4e831dd201163110beed78665b9bb902
taykim7 <th_77@naver.com>
2025-05-27 22:33:25 +0900

    웹스토리지에 데이터 저장 기능 추가
    - reducer를 통해 각 action이 일어날때마다 해당 객체를 로컬스토리지에 저장
    - useEffect를 통해 최초 로드시 로컬스토리지로부터 객체 가져옴
    - 그리고 INIT 액션 호출로 데이터 세팅
    - 또한 id 높은 값을 찾아내서 세팅함

--- onebite-react1/section13/src/App.jsx
+++ onebite-react1/section13/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useReducer, useRef, createContext } from 'react'
+import { useReducer, useRef, createContext, useEffect, act } from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import New from './pages/New'
@@ -12,52 +12,94 @@ import Edit from './pages/Edit'
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 // 4. "/edit" : 일기를 수정하는 Edit 페이지
 
-const mockData = [
-  {
-    id: 1,
-    createdDate: new Date('2025-05-15').getTime(),
-    emotionId: 1,
-    content: '1번 일기의 내용'
-  },
-  {
-    id: 2,
-    createdDate: new Date('2025-05-14').getTime(),
-    emotionId: 2,
-    content: '2번 일기의 내용'
-  },
-  {
-    id: 3,
-    createdDate: new Date('2025-04-15').getTime(),
-    emotionId: 3,
-    content: '3번 일기의 내용'
-  },
-]
+// const mockData = [
+//   {
+//     id: 1,
+//     createdDate: new Date('2025-05-15').getTime(),
+//     emotionId: 1,
+//     content: '1번 일기의 내용'
+//   },
+//   {
+//     id: 2,
+//     createdDate: new Date('2025-05-14').getTime(),
+//     emotionId: 2,
+//     content: '2번 일기의 내용'
+//   },
+//   {
+//     id: 3,
+//     createdDate: new Date('2025-04-15').getTime(),
+//     emotionId: 3,
+//     content: '3번 일기의 내용'
+//   },
+// ]
 
 function reducer(state, action) {
+  // state를 저장할 변수
+  let nextState;
+
   switch(action.type) {
+    case 'INIT':
+      return action.data;
     case 'CREATE':
-      return [action.data, ...state]
+      // return [action.data, ...state]
+      nextState = [action.data, ...state];
+      break;
     case 'UPDATE':
-      return state.map((item) => 
+      // return state.map((item) => 
+      //   String(item.id) === String(action.data.id)
+      //     ? action.data
+      //     : item
+      // );
+      nextState = state.map((item) => 
         String(item.id) === String(action.data.id)
           ? action.data
           : item
       );
+      break;
     case 'DELETE':
-      return state.filter((item) => 
+      // return state.filter((item) => 
+      //   String(item.id) !== String(action.id)
+      // );
+      nextState = state.filter((item) => 
         String(item.id) !== String(action.id)
       );
+      break;
     default:
       return state;
   }
+  // 로컬스토리지에 저장
+  localStorage.setItem('diary', JSON.stringify(nextState));
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(3);
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+  useEffect(()=>{
+    const storedDate = localStorage.getItem('diary');
+    if (!storedDate) return;
+    const parsedDate = JSON.parse(storedDate);
+
+    if (!Array.isArray(parsedDate)) return;
+
+    // 가장 id 가 높은 값을 찾아야함
+    let maxId = 0;
+    parsedDate.forEach(item => {
+      if(Number(item.id) > maxId) {
+        maxId = Number(item.id);
+      };
+    });
+
+    idRef.current = maxId + 1;
+    
+    dispatch({
+      type: 'INIT',
+      data: parsedDate,
+    })
+  }, []);
 
   // 새로운 일기 추가
   const onCreate = (createdDate, emotionId, content) => {
